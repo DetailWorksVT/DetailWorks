1a8842cbeac0017f54ebfb9655e9793d1a33627b
Mikael Andres <mikaelandres@Mikaels-MacBook-Pro.local>
2021-05-13 15:29:36 -0400

    spelling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,9 +7,6 @@ import AppointmentScheduler from "./AppointmentScheduler";
 export default function Home() {
   return (
     <>
-      <div className="home-container">
-        <img id="logo" src={Logo} width="225" alt="Detail Works Logo" title="Detail Works Logo"/>
-      </div>
       <AppointmentScheduler />
     </>
   );
