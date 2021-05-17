8cea19c3e721074f0818d82d1975200b7d448ed1
Mikael Andres <mikaelandres@Mikaels-MacBook-Pro.local>
2021-05-17 14:10:49 -0400

    0 still not working

--- client/src/components/AppointmentScheduler.js
+++ client/src/components/AppointmentScheduler.js
@@ -129,10 +129,9 @@ export default function AppointmentScheduler() {
         scheduleArrEight.push(appointment); //pushes existing appointments for that date at 8:00am into an array
         setScheduledEight(scheduleArrEight.length);
 
-        // if (availabilityCheck[appointment.appointmentDate].eightAM === 0) {
-        //   setUnavailableEight(true);
-        // } else 
-        if (
+        if (availabilityCheck[appointment.appointmentDate].eightAM === 0) {
+          setUnavailableEight(true);
+        } else if (
           scheduleArrEight.length ===
           ((availabilityCheck[appointment.appointmentDate] &&
             availabilityCheck[appointment.appointmentDate].eightAM) ||
@@ -147,10 +146,9 @@ export default function AppointmentScheduler() {
       ) {
         scheduleArrNoon.push(appointment); //pushes existing appointments for that date at 12:00pm into an array
         setScheduledNoon(scheduleArrNoon.length);
-        // if (availabilityCheck[appointment.appointmentDate].noon === 0) {
-        //   setUnavailableNoon(true);
-        // } else 
-        if (
+        if (availabilityCheck[appointment.appointmentDate].noon === 0) {
+          setUnavailableNoon(true);
+        } else if (
           scheduleArrNoon.length ===
           ((availabilityCheck[appointment.appointmentDate] &&
             availabilityCheck[appointment.appointmentDate].noon) ||
